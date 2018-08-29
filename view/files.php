114a04c77df80c0a8bea880a48f5271290947446
jericoluislua <jericoluislua@yahoo.com.ph>
2018-08-29 16:49:56 +0200

    working password thingy

--- view/files.php
+++ view/files.php
@@ -6,19 +6,28 @@
  * Time: 5:40 PM
  */
 
+if(!isset($_SESSION['openfiles'])){
 
-?>
-<p class="fileabt">Files that are secured by a password.</p>
-<form class="form-horizontal" action="/files/checkPassword" method="post">
-    <div class="component" data-html="true">
-        <div class="form-group">
-
+    ?>
+    <p class="fileabt">Files that are secured by a password.</p>
+    <form class="form-horizontal" action="/files/checkPassword" method="post">
+        <div class="component" data-html="true">
             <div class="form-group">
-                <label class="col-md-1 control-label" for="password">Password</label>
-                <div class="col-md-4">
-                    <input id="password" name="password" type="password" placeholder="Password" class="form-control input-md">
+
+                <div class="form-group">
+                    <label class="col-md-1 control-label" for="password">Password</label>
+                    <div class="col-md-4">
+                        <input id="password" name="password" type="password" placeholder="Password" class="form-control input-md" required>
+                    </div>
+                    <input id="filebtn" name="send" type="submit" class="btn" value="Show secured files">
                 </div>
-                <input id="filebtn" name="send" type="submit" class="btn" value="Show secured files">
             </div>
-        </div>
-</form>
+    </form>
+    <?php
+}
+else if(isset($_SESSION['openfiles'])){
+    ?>
+    <p>Secured Files</p>
+    <?php
+}
+?>
