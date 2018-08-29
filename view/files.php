277209ec2a856828beb8606c696d25af5b7808a8
jericoluislua <jericoluislua@yahoo.com.ph>
2018-08-29 15:54:09 +0200

    form finished

--- view/files.php
+++ view/files.php
@@ -6,4 +6,19 @@
  * Time: 5:40 PM
  */
 
-echo 'Files that are secured by a password.';
+
+?>
+<p class="fileabt">Files that are secured by a password.</p>
+<form class="form-horizontal" action="/user/doLogin" method="post">
+    <div class="component" data-html="true">
+        <div class="form-group">
+
+            <div class="form-group">
+                <label class="col-md-1 control-label" for="password">Password</label>
+                <div class="col-md-4">
+                    <input id="password" name="password" type="password" placeholder="Password" class="form-control input-md">
+                </div>
+                <input id="filebtn" name="send" type="submit" class="btn" value="Show secured files">
+            </div>
+        </div>
+</form>
