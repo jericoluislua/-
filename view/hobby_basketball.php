d47a81c66add46c9c9afc31c933b1167e15eeaf7
Jerico Lua <jericoluislua@yahoo.com.ph>
2018-10-08 00:16:58 +0200

    added stuff

--- view/hobby_basketball.php
+++ view/hobby_basketball.php
@@ -6,11 +6,20 @@
  * Time: 5:37 PM
  */
 ?>
-<input type="submit" name="return" value="Return" id="btnreturn" class="btnreturn" onclick="history.back(-1)"/>
+<button onclick="location.href = '/HOBBIES/';" class="btn btnreturn">
+    <i class="fa fa-angle-left"></i>
+    <span>return</span>
+</button>
 </div>
 <div class="videoinformation">
     <p>A mini compilation I did after I noticed I played pretty well.</p>
     <div class="resp-videcontainer">
         <iframe class="resp-iframe" src="https://www.youtube.com/embed/l1_KtRDLwr0?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+</div>
+<div class="videoinformation">
+    <p>A very mini compilation of my buzzer beaters.</p>
+    <div class="resp-videocontainer">
+        <iframe class="resp-iframe" src="https://www.youtube.com/embed/9tYd-jhdyh8?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     </div>
 </div>
--- view/hobby_cubing.php
+++ view/hobby_cubing.php
@@ -6,7 +6,12 @@
  * Time: 7:03 PM
  */
 ?>
-<form action="/HOBBIES" method="post">
-    <input type="submit" name="return" value="Return" id="btnreturn" class="btn btnreturn icon-arrow-left"/>
-</form>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/fNN88bEPqGo?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<button onclick="location.href = '/HOBBIES/';" class="btn btnreturn">Return</button>
+
+</div>
+<div class="videoinformation">
+    <p>A mini compilation I did after I noticed I played pretty well.</p>
+    <div class="resp-videcontainer">
+        <iframe class="resp-iframe" src="https://www.youtube.com/embed/fNN88bEPqGo?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+</div>
