7514046c7332b55cb868b47dd19a2535d62b44d5
Jerico Lua <jericoluislua@yahoo.com.ph>
2018-10-10 23:09:22 +0200

    tweaks

--- view/files.php
+++ view/files.php
@@ -34,45 +34,133 @@ if(!isset($_SESSION['openfiles'])){
 else if(isset($_SESSION['openfiles'])){
     ?>
     </div>
-    <div class="zeugnis">
-        <div class="row rfilegroup">
-                <div class="col groupname">
-                    <h2 class="h hzeugnis">Zeugnis</h2>
-                </div>
+    <div class="row rfilegroup zeugnis">
+        <h2 class="col groupname h hzeugnis">Zeugnisse</h2>
 
-                <div class="row innerrow rbwd">
-                    <h3 class="filename col-md-6">BWD</h3>
-                    <div class="filedetails col-md-6" align="right">
-                        <a href="../docs/zeugnisse/bwd/bwd_Zeugnis_Jerico_Lua.pdf" download>download</a>
-                    </div>
-                </div>
+        <div class="row rcontent rbwd">
+            <h4 class="filename col-md-6">BWD</h4>
+            <div class="filedetails filedetails-sho col-md-6" align="right">
+                <a href="/docs/zeugnisse/bwd/bwd_Zeugnis_Jerico_Lua.pdf" download>download</a>
+            </div>
+        </div>
 
-                <div class="row innerrow rgibb">
-                    <h3 class="filename col-md-6">gibb</h3>
-                    <div class="filedetails col-md-6" align="right">
-                        <a href="../docs/zeugnisse/gibb/gibb_Zeugnis_Jerico_Lua.pdf" download>download</a>
-                    </div>
-                </div>
+        <div class="row rcontent rgibb">
+            <h4 class="filename col-md-6">gibb</h4>
+            <div class="filedetails filedetails-sho col-md-6" align="right">
+                <a href="/docs/zeugnisse/gibb/gibb_Zeugnis_Jerico_Lua.pdf" download>download</a>
+            </div>
         </div>
     </div>
 
-    <div class="uek">
-        <div class="row rfilegroup rfilegroup-mid">
-                <div class="col groupname">
-                    <h2 class="h huek">ÜK</h2>
-                </div>
+    <div class="row rfilegroup rfilegroup-mid uek">
+        <h2 class="col groupname h huek">ÜKs</h2>
 
-                <div class="row innerrow ruek101">
-                    <h3 class="filename col-md-6"> M101 - Webauftritt erstellen und veröffentlichen</h3>
-                    <div class="filedetails coldl" align="right">
-                        <a title="grade" href="../public/images/mepic.JPG" download>download</a>
-                    </div>
-                    <div class="filedetails coldl" align="right">
-                        <a title="grade + teacher's feedback + daily journal" href="../public/images/mepic.JPG" download>download</a>
-                    </div>
-                </div>
+        <div class="row rcontent ruek101">
+            <h4 class="filename col-md-6">M101 - Webauftritt erstellen und veröffentlichen</h4>
+            <div class="filedetails filedetails-lon" align="right">
+                <a title="grade + teacher's feedback + daily journal" href="../public/images/test.JPG" download>download</a>
+            </div>
+            <div class="filedetails filedetails-lon" align="right">
+                <a title="grade" href="../public/images/mepic.JPG" download>download</a>
+            </div>
+        </div>
+
+        <div class="row rcontent ruek105">
+            <h4 class="filename col-md-6">M105 - Datenbanken mit SQL bearbeiten</h4>
+            <div class="filedetails filedetails-lon" align="right">
+                <a title="grade + teacher's feedback + daily journal" href="../public/images/test.JPG" download>download</a>
+            </div>
+            <div class="filedetails filedetails-lon" align="right">
+                <a title="grade" href="../public/images/mepic.JPG" download>download</a>
+            </div>
+        </div>
+
+        <div class="row rcontent ruek302">
+            <h4 class="filename col-md-10">M302 - Fortgeschrittene Funktionen von Office Werkzeuge nutzen</h4>
+            <div class="filedetails filedetails-lon" align="right">
+                <a title="grade + teacher's feedback + daily journal" href="../public/images/test.JPG" download>download</a>
+            </div>
+            <div class="filedetails filedetails-lon " align="right">
+                <a title="grade" href="../public/images/mepic.JPG" download>download</a>
+            </div>
+        </div>
+
+        <div class="row rcontent ruek304">
+            <h4 class="filename col-md-8">M304 - Einzelplatz-Computer in Betrieb nehmen</h4>
+            <div class="filedetails filedetails-lon" align="right">
+                <a title="grade + teacher's feedback + daily journal" href="../public/images/test.JPG" download>download</a>
+            </div>
+            <div class="filedetails filedetails-lon" align="right">
+                <a title="grade" href="../public/images/mepic.JPG" download>download</a>
+            </div>
+        </div>
+
+        <div class="row rcontent ruek305">
+            <h4 class="filename col-md-10">M305 - Betriebssysteme installieren, konfigurieren und installieren</h4>
+            <div class="filedetails filedetails-lon" align="right">
+                <a title="grade + teacher's feedback + daily journal" href="../public/images/test.JPG" download>download</a>
+            </div>
+            <div class="filedetails filedetails-lon " align="right">
+                <a title="grade" href="../public/images/mepic.JPG" download>download</a>
+            </div>
+        </div>
+
+        <div class="row rcontent ruek307">
+            <h4 class="filename col-md-8">M307 - Interaktive Webseite mit Formular erstellen</h4>
+            <div class="filedetails filedetails-lon" align="right">
+                <a title="grade + teacher's feedback + daily journal" href="../public/images/test.JPG" download>download</a>
+            </div>
+            <div class="filedetails filedetails-lon" align="right">
+                <a title="grade" href="../public/images/mepic.JPG" download>download</a>
+            </div>
+        </div>
+
+        <div class="row rcontent ruek337">
+            <h4 class="filename col-md-6">M337 - Mobile-Applikation realisieren</h4>
+            <div class="filedetails filedetails-lon" align="right">
+                <a title="grade + teacher's feedback + daily journal" href="../public/images/test.JPG" download>download</a>
+            </div>
+            <div class="filedetails filedetails-lon" align="right">
+                <a title="grade" href="../public/images/mepic.JPG" download>download</a>
+            </div>
         </div>
     </div>
+
+    <div class="row rfilegroup rfilegroup-mid zertifikat">
+        <h2 class="col groupname h hzertifikat">Zertifikate</h2>
+
+        <div class="row rfileundergroup rzertinf">
+            <h3 class="col undergroupname h hzertinf">Informatik</h3>
+
+            <div class="row rcontent rsap">
+                <h4 class="filename col-md-6">Business Process Integration with SAP</h4>
+
+                <div class="filedetails filedetails-lon" align="right">
+                    <a title="grade + teacher's feedback + daily journal" href="../public/images/test.JPG" download>download</a>
+                </div>
+            </div>
+
+            <div class="row rcontent rsap-abap">
+                <h4 class="filename col-md-6">SAP-Einführung in die ABAP Entwicklung</h4>
+
+                <div class="filedetails filedetails-lon" align="right">
+                    <a title="grade + teacher's feedback + daily journal" href="../public/images/test.JPG" download>download</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="row rfileundergroup rzertson">
+            <h3 class="col undergroupname h hzertson">Sonstige</h3>
+
+            <div class="row rcontent rabacus">
+                <h4 class="filename col-md-6">Abacus</h4>
+                <div class="filedetails filedetails-sho">
+                    <a title="grade + teacher's feedback + daily journal" href="../public/images/test.JPG" download>download</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <?php
 }
 ?>
