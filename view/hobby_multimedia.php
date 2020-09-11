a6c8362ab4afe52c03291e7942acbf1bc7b3de8b
jericoluislua <38457091+jericoluislua@users.noreply.github.com>
2020-09-11 11:00:36 +0200

    Update hobby_multimedia.php

--- view/hobby_multimedia.php
+++ view/hobby_multimedia.php
@@ -13,25 +13,7 @@
         </button>
     </div>
 </div>
-<p>
-    I have been speedcubing for <strong>2 years</strong> now. The date I learned how to solve a cube was <strong>November 13 2015</strong>. My personal record is <strong>20.384 seconds</strong>.
-</p>
-<div class="row"><!--for the float left-->
-    <div class="col-md-6 firstvideo">
-        <div class="videoinformation panel panel-default">
-            <div class="panel-heading">
-                <p>An old video of me speedcubing.</p>
-            </div>
-            <div class="embed-responsive embed-responsive-yt" align="middle">
-                <iframe class="resp-iframe" src="https://www.youtube.com/embed/fNN88bEPqGo?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="row rhobbies">
 
-    <button onclick="location.href = '/HOBBIES/basketball';" id="basketball" class="btn btnhobby" >b a s k e t b a l l</button>
-    <button onclick="location.href = '/HOBBIES/dancing';" id="dancing" class="btn btnhobby" >d a n c i n g</button>
-    <button onclick="location.href = '/HOBBIES/videogames';" id="videogames" class="btn btnhobby" >v i d e o g a m e s</button>
-    <button onclick="location.href = '/HOBBIES/cubing';" id="cubing" class="btn btnhobby" >c u b i n g</button>
-</div>
+<p>
+    In this page I will be showing you the photos I have taken and 2 short films I have edited.
+</p>
