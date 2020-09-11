0df3fd62f2be45f9eb01b8ea2b092388bd721f9c
jericoluislua <38457091+jericoluislua@users.noreply.github.com>
2020-09-11 11:19:01 +0200

    Grammar changes

--- view/website.php
+++ view/website.php
@@ -13,17 +13,17 @@
 ?>
 <!--add a future div-->
 <p>
-    What's up with the (color & background) scheme you might ask?
+    What's up with the (color & background) scheme, you might ask? 
     <br>
-    <strong>This is Vaporwave</strong>. This version of <span id="vppink">pink</span>, <span id="vpviol">violet</span>, <span id="vpblue">blue</span> and <span id="vpgree">green</span> is very much associated with it.
+    <strong>This is Vaporwave</strong>. This <span id="vppink">col</span><span id="vpviol">or p</span><span id="vpblue">ale</span><span id="vpgree">tte</span> is very much associated with it. 
     <br>
     Now you might be asking yourself, what is Vaporwave?
     <br>
     Vaporwave is a music genre I really like.
     <br>
-    How I think can be really <strong>extra</strong>, <strong>weird</strong> and <strong>out of the box</strong>, just like Vaporwave. A Vaporwave song is very <strong>unpredictable</strong> and never sounds the same.
+    How I think can be really <strong>extra</strong>, <strong>weird</strong>, and <strong>out of the box</strong>, just like Vaporwave. A Vaporwave song is very <strong>unpredictable</strong> and never sounds the same. 
     <br>
-    I don't want to waste this platform just to show you what I am capable to do but also to share my story.
+    I don't want to waste this platform just to show you what I am capable of but also to share my story.
     <br>
     <strong>Everything I do is with passion and is personal to me and I will always try my best and give it my all.</strong>
 </p>
