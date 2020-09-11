831b9d5d6774db7da7dabb37adbee6e93fd36963
jericoluislua <38457091+jericoluislua@users.noreply.github.com>
2020-09-11 10:52:24 +0200

    Update hobby_multimedia.php

--- view/hobby_multimedia.php
+++ view/hobby_multimedia.php
@@ -6,6 +6,28 @@
  * Time: 16:41
  */
 ?>
+    <div class="btnreturndiv">
+        <button onclick="location.href = '/HOBBIES/';" class="btn btnreturn btnreturn-ing">
+            <i class="fa fa-angle-left"></i>
+            <span>return</span>
+        </button>
+    </div>
+</div>
+<p>
+    I have been speedcubing for <strong>2 years</strong> now. The date I learned how to solve a cube was <strong>November 13 2015</strong>. My personal record is <strong>20.384 seconds</strong>.
+</p>
+<div class="row"><!--for the float left-->
+    <div class="col-md-6 firstvideo">
+        <div class="videoinformation panel panel-default">
+            <div class="panel-heading">
+                <p>An old video of me speedcubing.</p>
+            </div>
+            <div class="embed-responsive embed-responsive-yt" align="middle">
+                <iframe class="resp-iframe" src="https://www.youtube.com/embed/fNN88bEPqGo?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="row rhobbies">
 
     <button onclick="location.href = '/HOBBIES/basketball';" id="basketball" class="btn btnhobby" >b a s k e t b a l l</button>
