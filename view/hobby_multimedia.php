5e51d709504c30f3acc546aedac7e2ea9ac27163
jericoluislua <38457091+jericoluislua@users.noreply.github.com>
2020-09-11 10:47:42 +0200

    Update hobby_multimedia.php

--- view/hobby_multimedia.php
+++ view/hobby_multimedia.php
@@ -5,3 +5,11 @@
  * Date: 23.01.2019
  * Time: 16:41
  */
+</div>
+<div class="row rhobbies">
+
+    <button onclick="location.href = '/HOBBIES/basketball';" id="basketball" class="btn btnhobby" >b a s k e t b a l l</button>
+    <button onclick="location.href = '/HOBBIES/dancing';" id="dancing" class="btn btnhobby" >d a n c i n g</button>
+    <button onclick="location.href = '/HOBBIES/videogames';" id="videogames" class="btn btnhobby" >v i d e o g a m e s</button>
+    <button onclick="location.href = '/HOBBIES/cubing';" id="cubing" class="btn btnhobby" >c u b i n g</button>
+</div>
